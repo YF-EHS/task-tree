089a70dcd844e55afd0e51bf53e5c924bcf06d83
YF-EHS <uf.lanter@gmail.com>
2026-09-04 17:15:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// 【公司版專用】這個檔案不會被同步，只放在公司的網頁空間裡
+// 主程式碼（index.html）永遠共用同一份，這支 config.js 才是區分「公司」跟「個人」的地方
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAxA8ScL0CQiz4bxIqkRhAKFVLBfSoCwHQ",
+  authDomain: "task-tree-33560.firebaseapp.com",
+  projectId: "task-tree-33560",
+  storageBucket: "task-tree-33560.firebasestorage.app",
+  messagingSenderId: "934075104671",
+  appId: "1:934075104671:web:ab1b0fe45f61773806d1aa"
+};
